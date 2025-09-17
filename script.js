aa4fbd241fe46bb4a9cf029687db43d94ab267f5
gulshanbauddh <gulshanbncet@gmail.com>
2025-09-17 22:30:09 +0530

    code

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let boxs =document.querySelectorAll(".box");
-// boxs[0].innerText="A";
-i=1;
-boxs.forEach((box)=>{
-    box.innerText=i++;    
+let boxs = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+
+let turnO = true;
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxs.forEach((box) => {
+  box.innerText = i++;
 });
