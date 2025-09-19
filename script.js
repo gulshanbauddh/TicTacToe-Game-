486772b03e4cb51bf3dc850a5de5797de79e8660
gulshanbauddh <gulshanbncet@gmail.com>
2025-09-19 22:58:54 +0530

    new change

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxs = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
+let newGameBtn=document.querySelector('#newGame');
+let msgContainer = document.querySelector('.msg-container');
+let msg=document.querySelector('#msg-winner')
 
 let turnO = true;
 const winPatterns = [
@@ -36,8 +39,8 @@ const checkWinner = () => {
     let pos3Val = boxs[pattern[2]].innerText;
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val == pos2Val && pos2Val == pos3Val) {
-        alert("Winner");
-      }
+        console.log("winner", pos1Val);
+      } 
     }
   }
 };
