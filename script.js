338c1f439473e5c5b1c8d7775b43cf64f93a3284
gulshanbauddh <gulshanbncet@gmail.com>
2025-09-22 17:15:21 +0530

    fdsf

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-let boxs = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset");
-let newGameBtn=document.querySelector('#newGame');
-let msgContainer = document.querySelector('.msg-container');
-let msg=document.querySelector('#msg-winner')
+// let boxs = document.querySelectorAll(".box");
+// let resetBtn = document.querySelector("#reset");
+// let newGameBtn=document.querySelector('#newGame');
+// let msgContainer = document.querySelector('.msg-container');
+// let msg=document.querySelector('#msg-winner')
 
-let turnO = true;
-const winPatterns = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+// let turnO = true;
+// const winPatterns = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
 
-boxs.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (turnO) {
-      box.innerText = "0";
-      turnO = false;
-      box.disabled = true;
-    } else {
-      box.innerText = "X";
-      turnO = true;
-      box.disabled = true;
-    }
-    box.disabled = true;
-    checkWinner();
-  });
-});
+// boxs.forEach((box) => {
+//   box.addEventListener("click", () => {
+//     if (turnO) {
+//       box.innerText = "0";
+//       turnO = false;
+//       box.disabled = true;
+//     } else {
+//       box.innerText = "X";
+//       turnO = true;
+//       box.disabled = true;
+//     }
+//     box.disabled = true;
+//     checkWinner();
+//   });
+// });
 
-const checkWinner = () => {
-  for (let pattern of winPatterns) {
-    let pos1Val = boxs[pattern[0]].innerText;
-    let pos2Val = boxs[pattern[1]].innerText;
-    let pos3Val = boxs[pattern[2]].innerText;
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-      if (pos1Val == pos2Val && pos2Val == pos3Val) {
-        console.log("winner", pos1Val);
-      } 
-    }
-  }
-};
+// const checkWinner = () => {
+//   for (let pattern of winPatterns) {
+//     let pos1Val = boxs[pattern[0]].innerText;
+//     let pos2Val = boxs[pattern[1]].innerText;
+//     let pos3Val = boxs[pattern[2]].innerText;
+//     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+//       if (pos1Val == pos2Val && pos2Val == pos3Val) {
+//         console.log("winner", pos1Val);
+//       } 
+//     }
+//   }
+// };
+const num;
+num = 6;
+console.log(num);
